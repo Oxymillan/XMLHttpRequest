a293a021bb44f682f476505a10f34ecb5ffd9af6
JinEnomura <oxymje@gmail.com>
2020-04-25 20:02:20 +0100

    XMLHttpRequest, fetch and axios

---
+++ axios.js
@@ -0,0 +1,36 @@
+const getBtn = document.getElementById("get-btn");
+const postBtn = document.getElementById("post-btn");
+
+const getData = () => {
+  axios
+    .get("https://reqres.in/api/users")
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const sendData = () => {
+  axios
+    .post(
+      "https://reqres.in/api/register",
+      {
+        email: "eve.holt@reqres.in",
+        password: "pistol",
+      },
+      {
+        Accept: "text/turtle; charset=utf-8"
+      }
+    )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err, err.response);
+    });
+};
+
+getBtn.addEventListener("click", getData);
+postBtn.addEventListener("click", sendData);
